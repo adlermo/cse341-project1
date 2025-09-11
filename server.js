84802ce7a001233f2387d4685423455ac10e9b13
adlermo <adlerorteney@gmail.com>
2025-09-10 22:24:59 -0300

    feat: add basic Express server and routing
    - Created a new Express server in server.js
    - Set up a basic route in routes/index.js that responds with 'Hello World!'
    - Configured the server to listen on a specified port

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => { console.log(`Running on http://localhost:${port}`) });
+
+app.use(express.json());
+
+app.use('/', require('./routes'));
+
